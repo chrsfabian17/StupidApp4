44ee8838841dffdf08fe3573664f68d192ff553d
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-08 09:25:36 -0600

    Added dot

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,17 +1,29 @@
 package stupid.controller;
 
-public class StupidController 
+import java.util.Scanner;
+
+public class StupidController
 {
 	private String myName;
-	
-	public StupidController() 
+	private Scanner inputReader;
+
+	public StupidController()
 	{
 		myName = "Lady Killer";
+		inputReader = new Scanner(System.in);
+	}
+
+	public void start()
+	{
+		System.out.println("Im not stupid!");
+		System.out.println(myName);
+		askQuestions();
 	}
 	
- public void start()
- {
-	System.out.println("Im not stupid!");
-    System.out.println(myName);
- }
+	private void askQuestions()
+	{
+	System.out.println("Do you like Chick-Fil-a?");
+	String input = inputReader.nextLine();
+	System.out.println("Oh, you said:" + input + " to Chick-Fil-a");
+	}
 }
