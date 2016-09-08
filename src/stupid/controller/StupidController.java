8063f65d10c23f5db3471993e9abdb5c6ae11417
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-08 08:39:44 -0600

    Added my name to code.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -2,9 +2,16 @@ package stupid.controller;
 
 public class StupidController 
 {
+	private String myName;
+	
+	public StupidController() 
+	{
+		myName = "Lady Killer";
+	}
+	
  public void start()
  {
-
 	System.out.println("Im not stupid!");
+    System.out.println(myName);
  }
 }
