22061ab4d69aab2ab0ddef304589681fc8ece9f5
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-12 09:48:49 -0600

    Asked more stupid questions

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -25,5 +25,35 @@ public class StupidController
 	System.out.println("Do you like Chick-Fil-a?");
 	String input = inputReader.nextLine();
 	System.out.println("Oh, you said:" + input + " to Chick-Fil-a");
-	}
+	
+	System.out.println("My next question is this: Whats your favorite car?");
+	String carInput = inputReader.next();
+	System.out.println("okay you typed:" + carInput);
+	inputReader.nextLine(); //Used to consume unneeded input
+	
+	System.out.println("Now, what is your favorite color?");
+	String colorInput = inputReader.nextLine();
+	System.out.println("Oh," + colorInput + " is your favorite color.");
+	
+	System.out.println("How old are you?");
+	Integer ageInput = inputReader.nextInt(); 
+	System.out.println("Oh, so you are:" + ageInput);
+	
+	System.out.println("What is your favorite food?");
+    String foodInput = inputReader.next();
+    System.out.println("Okay, you typed this:" + foodInput);
+    if(!foodInput.equals(""))
+    {
+    	System.out.println("Okay, you typed this:" + foodInput);
+    }
+    
+    System.out.println("How many toes do you have?");
+    int someInput = inputReader.nextInt();
+    System.out.println("OK, you have " + someInput + " toes.");
+    inputReader.nextLine();
+	
+	
+	
+    }
+	
 }
